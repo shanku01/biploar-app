b8b4e8e96cbe4bdc92e15252ff60ced3c28ece58
Shashank Pradhan <shashankpradhan911@gmail.com>
2020-09-23 18:31:41 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ImgList from './Components/ImgList';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	constructor() {
+		super();
+		//all the images will load in imgs so that the app can you the data
+		this.state = {
+			imgs: []
+		};
+		//Binding fuction with class so they can use "this"
+		this.collection1 =this.collection1.bind(this);
+		this.collection2 =this.collection2.bind(this);
+	}
+	//loading images on page by default
+	componentDidMount() {
+		axios
+		.get('https://api.unsplash.com/collections/1580860/photos/?per_page=30&client_id=nMo0cw4x_jITHjIOLUHWgcTekdxKfSmU0y_uLMqDNnI')
+		.then(data => {
+			this.setState({ imgs: data.data });
+		})
+		.catch(err => {
+			console.log('Error happened during fetching!', err);
+		});
+	}
+	//loading images by choice
+	collection1(){
+		axios
+		.get('https://api.unsplash.com/collections/1580860/photos/?per_page=30&client_id=nMo0cw4x_jITHjIOLUHWgcTekdxKfSmU0y_uLMqDNnI')
+		.then(data => {
+			this.setState({ imgs: data.data });
+		})
+		.catch(err => {
+			console.log('Error happened during fetching!', err);
+		});
+	}
+	collection2(){
+		axios
+		.get('https://api.unsplash.com/collections/139386/photos/?per_page=30&client_id=nMo0cw4x_jITHjIOLUHWgcTekdxKfSmU0y_uLMqDNnI')
+		.then(data => {
+			this.setState({ imgs: data.data });
+		})
+		.catch(err => {
+			console.log('Error happened during fetching!', err);
+		});
+	}
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">Upsplash Api test</h1><br/><br/>
+						<button  className='search-button' onClick={this.collection1}>Collection1</button>
+						<button  className='search-button' onClick={this.collection2}>Collection2</button>
+					</div>
+				</div>
+				<div className="main-content">
+					<ImgList data={this.state.imgs} />
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
---
+++ src/Components/Img.js
@@ -0,0 +1,10 @@
+//image component
+import React from 'react';
+
+const Img = props => (
+  <li className="img-wrap">
+    <img src={props.url} alt=""/>
+  </li>
+);
+
+export default Img;
